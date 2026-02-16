9fee4dbab82181230f9b1a0f5b8c191967968250
Atharva Sharma <atharvsharma401@gmail.com>
2026-02-16 23:55:19 +0530

    Implement Stack Using Queues

---
+++ Stack/Implement Stack Using Queues.py
@@ -0,0 +1,26 @@
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        for i in range(len(self.q) - 1):
+            self.push(self.q.popleft()) #to pop the left most element of the array 
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[-1] #to display the topmost element 
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
