bdf6587fc466ebb9270097897684389ddb5dae03
Atharva Sharma <atharvsharma401@gmail.com>
2026-03-18 14:57:47 +0530

    Implement Trie Prefix Tree

--- Tries/Implement Trie Prefix Tree.py
+++ Tries/Implement Trie Prefix Tree.py
@@ -1,17 +1,37 @@
-class Trie:
-
+class TrieNode:
     def __init__(self):
-        self.children 
-        self.
+        self.children = {}
+        self.endofWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endofWord = True
 
     def search(self, word: str) -> bool:
-        
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.endofWord  
 
     def startsWith(self, prefix: str) -> bool:
-        
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.children:
+                return False 
+            cur = cur.children[c]
+        return True 
 
 
 # Your Trie object will be instantiated and called as such:
